9c63d42ffeed15c69b84d1ec78e60902b7e3aff0
Sumit kumar <sumitkumar808393@gmail.com>
2025-09-18 22:29:41 +0530

    Spring JDBC

--- src/main/java/com/springcore/springJdbc/App.java
+++ src/main/java/com/springcore/springJdbc/App.java
@@ -1,5 +1,9 @@
 package com.springcore.springJdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Program Started.................." );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/springJdbc/jdbcConfig.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        System.out.println(template);
+        
+        //Insert Query
+        String query = "insert into student (id,name,address) values (?,?,?)";
+        
+        int result = template.update(query,12345,"Sumit Kumar","Nalanda");
+        System.out.println("No of row Inserted : " + result);
+        
     }
 }
